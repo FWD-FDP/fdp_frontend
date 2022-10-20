34696e08a5eafe081148b25270e4a88fdfbd2d0f
borel-T <borel-T@users.noreply.github.com>
2022-10-20 18:23:50 +0300

    added homepage and updated entry index

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
+import HomePage from "./containers/homepage";
 
 const App = () => {
-    return(
-        <h1>Hello world</h1>
-    )
-}
+  return <HomePage />;
+};
 
 export default App;
-
