5e9eea26e5ec9ab79595f3d6dd38ec504f0b84a1
borel-T <borel-T@users.noreply.github.com>
2022-11-06 22:22:54 +0300

    app routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import HomePage from "./containers/homepage";
+import HomePage from "./containers/homePage";
+import ContactPage from "./containers/contactPage";
+import Search from "./containers/search";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/mainLayout";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <BrowserRouter basename="fdp">
+      <MainLayout>
+        <Routes>
+          <Route path="*" element={<HomePage />}></Route>
+          <Route path="home" element={<HomePage />}></Route>
+          <Route path="search/*" element={<Search />}></Route>
+          <Route path="contacts" element={<ContactPage />}></Route>
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
