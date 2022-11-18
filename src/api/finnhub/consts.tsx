d43a2c3db07e9827ed8f4b32a2df709200f0f030
AKurmazov <akurmazov2001@gmail.com>
2022-11-18 23:31:56 +0300

    Do not take prefix into account

--- src/api/finnhub/consts.tsx
+++ src/api/finnhub/consts.tsx
@@ -1,4 +1,4 @@
 import { getConfig } from '@ijl/cli'
 
-export const API_KEY = getConfig().fdp["apiKey"];
+export const API_KEY = getConfig().apiKey;
 export const SOCKET_URL = "wss://ws.finnhub.io?token=" + API_KEY;
